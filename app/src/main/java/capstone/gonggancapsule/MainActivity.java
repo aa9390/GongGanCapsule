f495bc14e23e180978cd715ec64f8bbbab78c665
aa9390 <aa9390>
2018-05-07 16:56:40 +0900

    [소연] 메인화면 터치 시 로봇 생성 ar

--- app/src/main/java/capstone/gonggancapsule/MainActivity.java
+++ app/src/main/java/capstone/gonggancapsule/MainActivity.java
@@ -1,12 +1,7 @@
 package capstone.gonggancapsule;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.opengl.GLES20;
@@ -16,14 +11,12 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -45,6 +38,10 @@ import com.google.ar.core.PointCloud;
 import com.google.ar.core.Session;
 import com.google.ar.core.Trackable;
 import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
@@ -66,6 +63,7 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
 
     boolean permissionCheck = false;
+
     @BindView(R.id.database)
     ImageButton database;
     private LocationManager locationManager;
@@ -75,16 +73,12 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
     private final BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
     private final PlaneRenderer planeRenderer = new PlaneRenderer();
     private final PointCloudRenderer pointCloudRenderer = new PointCloudRenderer();
+    private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
 
-    // 현재는 앵커(Object)를 만들지 않았기 때문에 필요없는 코드.
+    // 앵커 관련 코드
     private final ObjectRenderer virtualObject = new ObjectRenderer();
     private final ObjectRenderer virtualObjectShadow = new ObjectRenderer();
-
-
-    // Temporary matrix allocated here to reduce number of allocations for each frame.
     private final float[] anchorMatrix = new float[16];
-
-    // Anchors created from taps used for object placing.
     private final ArrayList<Anchor> anchors = new ArrayList<>();
 
     private TapHelper tapHelper;
@@ -125,22 +119,6 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
         setContentView( R.layout.activity_main );
         ButterKnife.bind( this );
 
-        //위치 받아오기
-        GPSTracker mGPS = new GPSTracker(this);
-
-        //위치 받아오는지 확인하기 위한 임시코드
-        TextView text = (TextView) findViewById(R.id.longi);
-        TextView text2 = (TextView) findViewById(R.id.lati);
-
-        if(mGPS.canGetLocation ){
-            mGPS.getLocation();
-            text.setText("Lat"+mGPS.getLatitude());
-            text2.setText("Lon"+mGPS.getLongitude());
-        }else{
-            text.setText("Unabletofind");
-            System.out.println("Unable");
-        }
-
         //splash 화면 띄우기
         startActivity(new Intent(this, SplashActivity.class));
 
@@ -148,6 +126,8 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
         surfaceView = findViewById( R.id.surfaceview );
         displayRotationHelper = new DisplayRotationHelper(/*context=*/ this );
         tapHelper = new TapHelper(/*context=*/ this);
+
+        surfaceView.setOnTouchListener(tapHelper);
 
         // 플로팅 버튼 id 가져오기, 클릭 리스너 선언
         floatingBtn = findViewById( R.id.floatingBtn );
@@ -163,7 +143,6 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
 
         longi = findViewById( R.id.longi );
         lati = findViewById( R.id.lati );
-
 
         // 메인 화면 초기화
         initView();
@@ -207,7 +186,6 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
         surfaceView.setEGLConfigChooser( 8, 8, 8, 8, 16, 0 ); // Alpha used for plane blending.
         surfaceView.setRenderer( this );
         surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-
     }
 
     private File createImageFile() throws IOException {
@@ -227,7 +205,19 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
 
         Toast.makeText( this, "on Pause", Toast.LENGTH_SHORT ).show();
 
+        Session session = null;
+        try {
+            session = new Session( this );
+        } catch (UnavailableArcoreNotInstalledException e) {
+            e.printStackTrace();
+        } catch (UnavailableApkTooOldException e) {
+            e.printStackTrace();
+        } catch (UnavailableSdkTooOldException e) {
+            e.printStackTrace();
+        }
+        displayRotationHelper.onPause();
         surfaceView.onPause();
+        session.pause();
     }
 
     @Override
@@ -238,8 +228,22 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
         if (!permissionCheck) getPermission();
         else {
             Toast.makeText( this, "on Resume", Toast.LENGTH_SHORT ).show();
-            session = new Session( this );
-            session.resume();
+            if (session==null) {
+                try {
+                    session = new Session( this );
+                } catch (UnavailableArcoreNotInstalledException e) {
+                    e.printStackTrace();
+                } catch (UnavailableApkTooOldException e) {
+                    e.printStackTrace();
+                } catch (UnavailableSdkTooOldException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                session.resume();
+            } catch (CameraNotAvailableException e) {
+                e.printStackTrace();
+            }
             surfaceView.onResume();
             displayRotationHelper.onResume();
         }
@@ -382,12 +386,10 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
 
       virtualObject.createOnGlThread(/*context=*/ this, "models/andy.obj", "models/andy.png");
       virtualObject.setMaterialProperties(0.0f, 2.0f, 0.5f, 6.0f);
-//
       virtualObjectShadow.createOnGlThread(
           /*context=*/ this, "models/andy_shadow.obj", "models/andy_shadow.png");
       virtualObjectShadow.setBlendMode( ObjectRenderer.BlendMode.Shadow);
       virtualObjectShadow.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f);
-//
         } catch (IOException e) {
 //            Log.e(TAG, "Failed to read an asset file", e);
         }
@@ -406,17 +408,139 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
     // GLSurfaceView.onPause()가 호출되면 GLSurfaceView.Renderer interface의 abstract method 들의 호출이 중단되고,
     // onResume()이 호출되면 onSurfaceCreated()부터 다시 호출이 된다.
     // 즉, surface가 다시 생성된다.
+
+    // 작동되는 코드
+
+//    @Override
+//    public void onDrawFrame(GL10 gl) {
+//        // Clear screen to notify driver it should not load any pixels from previous frame.
+//        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+//
+//        if (session == null) {
+//            return;
+//        }
+//        // Notify ARCore session that the view size changed so that the perspective matrix and
+//        // the video background can be properly adjusted.
+//        displayRotationHelper.updateSessionIfNeeded(session);
+//
+//        try {
+//            session.setCameraTextureName(backgroundRenderer.getTextureId());
+//
+//            // Obtain the current frame from ARSession. When the configuration is set to
+//            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+//            // camera framerate.
+//            Frame frame = session.update();
+//            Camera camera = frame.getCamera();
+//
+//            // Handle taps. Handling only one tap per frame, as taps are usually low frequency
+//            // compared to frame rate.
+//
+//            MotionEvent tap = tapHelper.poll();
+//            if (tap != null && camera.getTrackingState() == TrackingState.TRACKING) {
+//                for (HitResult hit : frame.hitTest(tap)) {
+//                    // Check if any plane was hit, and if it was hit inside the plane polygon
+//                    Trackable trackable = hit.getTrackable();
+//                    // Creates an anchor if a plane or an oriented point was hit.
+//                    if ((trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose()))
+//                            || (trackable instanceof Point
+//                            && ((Point) trackable).getOrientationMode()
+//                            == OrientationMode.ESTIMATED_SURFACE_NORMAL)) {
+//                        // Hits are sorted by depth. Consider only closest hit on a plane or oriented point.
+//                        // Cap the number of objects created. This avoids overloading both the
+//                        // rendering system and ARCore.
+//                        if (anchors.size() >= 20) {
+//                            anchors.get(0).detach();
+//                            anchors.remove(0);
+//                        }
+//                        // Adding an Anchor tells ARCore that it should track this position in
+//                        // space. This anchor is created on the Plane to place the 3D model
+//                        // in the correct position relative both to the world and to the plane.
+//                        anchors.add(hit.createAnchor());
+//                        break;
+//                    }
+//                }
+//            }
+//
+//            // Draw background.
+//            backgroundRenderer.draw(frame);
+//
+//            // If not tracking, don't draw 3d objects.
+//            if (camera.getTrackingState() == TrackingState.PAUSED) {
+//                return;
+//            }
+//
+//            // Get projection matrix.
+//            float[] projmtx = new float[16];
+//            camera.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f);
+//
+//            // Get camera matrix and draw.
+//            float[] viewmtx = new float[16];
+//            camera.getViewMatrix(viewmtx, 0);
+//
+//            // Compute lighting from average intensity of the image.
+//            // The first three components are color scaling factors.
+//            // The last one is the average pixel intensity in gamma space.
+//            final float[] colorCorrectionRgba = new float[4];
+//            frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
+//
+//            // Visualize tracked points.
+//            PointCloud pointCloud = frame.acquirePointCloud();
+//            pointCloudRenderer.update(pointCloud);
+//            pointCloudRenderer.draw(viewmtx, projmtx);
+//
+//            // Application is responsible for releasing the point cloud resources after
+//            // using it.
+//            pointCloud.release();
+//
+//            // Check if we detected at least one plane. If so, hide the loading message.
+//            if (messageSnackbarHelper.isShowing()) {
+//                for (Plane plane : session.getAllTrackables(Plane.class)) {
+//                    if (plane.getType() == Plane.Type.HORIZONTAL_UPWARD_FACING
+//                            && plane.getTrackingState() == TrackingState.TRACKING) {
+//                        messageSnackbarHelper.hide(this);
+//                        break;
+//                    }
+//                }
+//            }
+//
+//            // Visualize planes.
+//            planeRenderer.drawPlanes(
+//                    session.getAllTrackables(Plane.class), camera.getDisplayOrientedPose(), projmtx);
+//
+//            // Visualize anchors created by touch.
+//            float scaleFactor = 1.0f;
+//            for (Anchor anchor : anchors) {
+//                if (anchor.getTrackingState() != TrackingState.TRACKING) {
+//                    continue;
+//                }
+//                // Get the current pose of an Anchor in world space. The Anchor pose is updated
+//                // during calls to session.update() as ARCore refines its estimate of the world.
+//                anchor.getPose().toMatrix(anchorMatrix, 0);
+//
+//                // Update and draw the model and its shadow.
+//                virtualObject.updateModelMatrix(anchorMatrix, scaleFactor);
+//                virtualObjectShadow.updateModelMatrix(anchorMatrix, scaleFactor);
+//                virtualObject.draw(viewmtx, projmtx, colorCorrectionRgba);
+//                virtualObjectShadow.draw(viewmtx, projmtx, colorCorrectionRgba);
+//            }
+//
+//        } catch (Throwable t) {
+//            // Avoid crashing the application due to unhandled exceptions.
+////            Log.e(TAG, "Exception on the OpenGL thread", t);
+//        }
+//    }
+
     @Override
     public void onDrawFrame(GL10 gl) {
         // Clear screen to notify driver it should not load any pixels from previous frame.
-        GLES20.glClear( GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT );
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-        // save 버튼을 클릭하고 메인화면 오류없이 돌아가기
-        if(session == null) {
+        if (session == null) {
             return;
         }
-
-        displayRotationHelper.updateSessionIfNeeded( session );
+        // Notify ARCore session that the view size changed so that the perspective matrix and
+        // the video background can be properly adjusted.
+        displayRotationHelper.updateSessionIfNeeded(session);
 
         try {
             session.setCameraTextureName(backgroundRenderer.getTextureId());
@@ -476,7 +600,7 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
             // The first three components are color scaling factors.
             // The last one is the average pixel intensity in gamma space.
             final float[] colorCorrectionRgba = new float[4];
-//            frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
+            frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
 
             // Visualize tracked points.
             PointCloud pointCloud = frame.acquirePointCloud();
@@ -487,16 +611,16 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
             // using it.
             pointCloud.release();
 
-//            // Check if we detected at least one plane. If so, hide the loading message.
-//            if (messageSnackbarHelper.isShowing()) {
-//                for (Plane plane : session.getAllTrackables(Plane.class)) {
-//                    if (plane.getType() == com.google.ar.core.Plane.Type.HORIZONTAL_UPWARD_FACING
-//                            && plane.getTrackingState() == TrackingState.TRACKING) {
-//                        messageSnackbarHelper.hide(this);
-//                        break;
-//                    }
-//                }
-//            }
+            // Check if we detected at least one plane. If so, hide the loading message.
+            if (messageSnackbarHelper.isShowing()) {
+                for (Plane plane : session.getAllTrackables(Plane.class)) {
+                    if (plane.getType() == Plane.Type.HORIZONTAL_UPWARD_FACING
+                            && plane.getTrackingState() == TrackingState.TRACKING) {
+                        messageSnackbarHelper.hide(this);
+                        break;
+                    }
+                }
+            }
 
             // Visualize planes.
             planeRenderer.drawPlanes(
@@ -524,6 +648,7 @@ public class MainActivity extends AppCompatActivity implements GLSurfaceView.Ren
 //            Log.e(TAG, "Exception on the OpenGL thread", t);
         }
     }
+
 
 
     @OnClick(R.id.database)
